48321cc8d7b7268432e6be3082a8ddfe65b53638
yasabri <yasabri04@gmail.com>
2024-03-10 12:23:55 -0700

    me

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Represent an amenit
+        name (str): The name of the amenity.
+    """
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines the City class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represent a city
+        state_id (str): id state
+        name (str): city name
+    """
+
+    state_id = ""
+    name = ""
