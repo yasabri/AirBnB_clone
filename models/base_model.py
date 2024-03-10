da263951223b19982c9b6e19b4dd2f29a8d285b5
yasabri <yasabri04@gmail.com>
2024-03-10 11:03:55 -0700

    me

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
+""" Class BaseModel """
 from datetime import datetime
 from uuid import uuid4
 import models
 
 
 class BaseModel:
-
+    """ construct """
 
     def __init__(self, *args, **kwargs):
-
+        """ Construct """
         if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
@@ -31,17 +32,17 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
-
+        """ String """
         return('[' + type(self).__name__ + '] (' + str(self.id) +
                ') ' + str(self.__dict__))
 
     def save(self):
-
+        """ save function """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-
+        """ Return a dictonary """
         aux_dict = self.__dict__.copy()
         aux_dict['__class__'] = self.__class__.__name__
         aux_dict['created_at'] = self.created_at.isoformat()
